c6557221f30080d3d3142ead7f0fcd70e9bcbc93
Soab42 <108150913+Soab42@users.noreply.github.com>
2022-11-07 23:35:39 +0600

    published

--- src/page/DailyCalc/Daily.js
+++ src/page/DailyCalc/Daily.js
@@ -1,3 +1,4 @@
+import moment from "moment";
 import React, { useState, useEffect } from "react";
 
 import classes from "../../styles/Daily.module.css";
@@ -9,7 +10,7 @@ export default function Daily() {
   const [day, setDay] = useState(0);
   const [principal, setPrincipal] = useState(0);
   const [lastoutstanding, setLastoutstanding] = useState(0);
-  const [finalservicecharge, setFinalservicecharge] = useState([]);
+  const [results, setResults] = useState([]);
   const [date, setDate] = useState(new Date());
   const [date2, setDate2] = useState(new Date());
   const [duration, setDuration] = useState();
@@ -17,11 +18,53 @@ export default function Daily() {
     setDay(
       Math.abs((new Date(date2) - new Date(date)) / (24 * 60 * 60 * 1000))
     );
-    const service = outstanding * (rate / (30.417 * duration));
+    const service = outstanding * (rate / 365);
     const charge = (service / 100) * day;
-    console.log(duration);
-    setServicecharge(charge.toFixed(2));
-  }, [date, date2, outstanding, rate, duration, day]);
+    console.log(date);
+    setServicecharge(Math.ceil(charge));
+    setPrincipal(recoverable - servicecharge);
+    setLastoutstanding(outstanding - principal);
+  }, [
+    date,
+    date2,
+    outstanding,
+    rate,
+    duration,
+    day,
+    principal,
+    servicecharge,
+    recoverable,
+  ]);
+  const result = () => {
+    setResults([
+      {
+        outstanding,
+        rate,
+        principal,
+        servicecharge,
+        recoverable,
+        duration,
+        lastoutstanding,
+        day,
+      },
+    ]);
+  };
+  const reset = () => {
+    setOutstanding(0);
+    setRate(0);
+    setRecoverable(0);
+    setDuration(0);
+  };
+  const autofill = () => {
+    setOutstanding(100000);
+    setDate(moment(new Date(date)).format("YYYY-MM-DD"));
+    setDate2(
+      moment(new Date(date).setDate(date.getDate() + 30)).format("YYYY-MM-DD")
+    );
+    setRate(24);
+    setRecoverable(9500);
+    setDuration(12);
+  };
   return (
     <div>
       <div
@@ -34,89 +77,139 @@ export default function Daily() {
       >
         Daily Interest Calculation
       </div>
-      <div className={classes.form}>
-        <label className={classes.label}>Loan Outstanding</label>
-        <input
-          className={classes.input}
-          type="text"
-          onChange={(e) => setOutstanding(e.target.value)}
-        ></input>
-        <label className={classes.label}>Previous Month Collection Date</label>
-        <input
-          className={classes.input}
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        ></input>
-        <label className={classes.label}>This Month Collection Date</label>
-        <input
-          className={classes.input}
-          type="date"
-          onChange={(e) => setDate2(e.target.value)}
-        ></input>
-        <label className={classes.label}>Interest Rate</label>
-        <input
-          className={classes.input}
-          type="text"
-          onChange={(e) => setRate(e.target.value)}
-        ></input>
-        <label className={classes.label}>Recoverable Amount</label>
-        <input
-          className={classes.input}
-          type="text"
-          onChange={(e) => setRecoverable(e.target.value)}
-        ></input>
-        <label className={classes.label}>Loan Durtion</label>
-        <input
-          className={classes.input}
-          type="text"
-          onChange={(e) => setDuration(e.target.value)}
-        ></input>
-        <button className={classes.btn} type="button" value={"Calculate"}>
-          Calculate
-        </button>
-      </div>
-      <div className={classes.result}>
-        <label className={classes.label}>Opening balance</label>
-        <input
-          className={classes.input}
-          disabled
-          type={"text"}
-          value={outstanding}
-        />
-        <label className={classes.label}>Interest Rate</label>
+      <div className={classes.frame}>
+        <div className={classes.form}>
+          <label className={classes.label}>Loan Outstanding</label>
+          <input
+            className={classes.input}
+            type="text"
+            value={outstanding}
+            onChange={(e) => setOutstanding(e.target.value)}
+          ></input>
+          <label className={classes.label}>
+            Previous Month Collection Date
+          </label>
+          <input
+            className={classes.input}
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          ></input>
+          <label className={classes.label}>This Month Collection Date</label>
+          <input
+            className={classes.input}
+            value={date2}
+            type="date"
+            onChange={(e) => setDate2(e.target.value)}
+          ></input>
+          <label className={classes.label}>Interest Rate</label>
+          <input
+            className={classes.input}
+            value={rate}
+            type="text"
+            onChange={(e) => setRate(e.target.value)}
+          ></input>
+          <label className={classes.label}>Recoverable Amount</label>
+          <input
+            className={classes.input}
+            value={recoverable}
+            type="text"
+            onChange={(e) => setRecoverable(e.target.value)}
+          ></input>
+          <label className={classes.label}>Loan Durtion</label>
+          <input
+            className={classes.input}
+            type="text"
+            value={duration}
+            onChange={(e) => setDuration(e.target.value)}
+          ></input>
+          <button
+            className={classes.btn}
+            type="button"
+            value={"Calculate"}
+            onClick={result}
+          >
+            Calculate
+          </button>
+          <button
+            className={classes.btn}
+            type="button"
+            value={"Calculate"}
+            onClick={autofill}
+          >
+            Autofill
+          </button>
+          <button
+            className={classes.btn}
+            type="button"
+            value={"Calculate"}
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </div>
+        {outstanding > 0
+          ? results.map((x) => {
+              return (
+                <div className={classes.result}>
+                  <label className={classes.label}>Opening balance</label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.outstanding}
+                  />
+                  <label className={classes.label}>Interest Rate</label>
 
-        <input className={classes.input} disabled type={"text"} value={rate} />
-        <label className={classes.label}>Calculation Day</label>
-        <input className={classes.input} disabled type={"text"} value={day} />
-        <label className={classes.label}>This Month service charge</label>
-        <input
-          className={classes.input}
-          disabled
-          type={"text"}
-          value={servicecharge}
-        />
-        <label className={classes.label}>This month principle</label>
-        <input
-          className={classes.input}
-          disabled
-          type={"text"}
-          value={recoverable - servicecharge}
-        />
-        <label className={classes.label}>This month recoverable</label>
-        <input
-          className={classes.input}
-          disabled
-          type={"text"}
-          value={servicecharge + principal}
-        />
-        <label className={classes.label}>Closing balance</label>
-        <input
-          className={classes.input}
-          disabled
-          type={"text"}
-          value={outstanding - recoverable + servicecharge}
-        />
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.rate}
+                  />
+                  <label className={classes.label}>Calculation Day</label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.day}
+                  />
+                  <label className={classes.label}>
+                    This Month service charge
+                  </label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.servicecharge}
+                  />
+                  <label className={classes.label}>This month principle</label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.principal}
+                  />
+                  <label className={classes.label}>
+                    This month recoverable
+                  </label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.recoverable}
+                  />
+                  <label className={classes.label}>Closing balance</label>
+                  <input
+                    className={classes.input}
+                    disabled
+                    type={"text"}
+                    value={x.lastoutstanding}
+                  />
+                </div>
+              );
+            })
+          : null}
       </div>
     </div>
   );
